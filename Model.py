342ba19b8fffaef44bab94c8fcdacc40fd3a8765
Zer0-0ne-01 <nikita1992sir@gmail.com>
2026-09-05 12:02:21 +0500

    исправлена ошибка с one hot encoding

--- Model.py
+++ Model.py
@@ -20,6 +20,8 @@ def pred(train_addres = "./student-por.csv", data_addres = "./data.csv"):
     rf.fit(df,target)
 
     p_df = pd.read_excel(data_addres)
+    p_df = pd.get_dummies(df,drop_first=True)
+
     threshold = 0.61
     proba =rf.predict(p_df)
 
